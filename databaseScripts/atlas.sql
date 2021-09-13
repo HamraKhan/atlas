a3cc3617ac5ec832c492be80346103de0e376026
Hamra Khan <hamra111@gmail.com>
2021-09-13 21:44:37 +0100

    updated sql statements for creating atlas database tables.

--- databaseScripts/atlas.sql
+++ databaseScripts/atlas.sql
@@ -1,24 +1,19 @@
-CREATE DATABASE atlas;
-
-CREATE TABLE IF NOT EXISTS `atlas`.`customer` (
-    `customer_id` INT NOT NULL AUTO_INCREMENT,
-    `full_name` VARCHAR(150) NOT NULL,
-    `email` VARCHAR(255),
-    `contact_number` VARCHAR(75),
-    PRIMARY KEY (`customer_id`) 
+CREATE TABLE IF NOT EXISTS customer (
+    customer_id SERIAL PRIMARY KEY,
+    full_name VARCHAR(150) NOT NULL,
+    email VARCHAR(255),
+    contact_number VARCHAR(75) 
 );
 
-CREATE TABLE IF NOT EXISTS `atlas`.`table_details` (
-    `table_id` INT NOT NULL AUTO_INCREMENT,
-    `seat_count` INT NOT NULL,
-    PRIMARY KEY (`table_id`) 
+CREATE TABLE IF NOT EXISTS table_details (
+    table_id SERIAL PRIMARY KEY,
+    seat_count INT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS `atlas`.`booking` (
-    `booking_id` INT NOT NULL AUTO_INCREMENT,
-    `customer_id` VARCHAR(150) NOT NULL,
-    `table_id` VARCHAR(150) NOT NULL,
-    `party_size` INT NOT NULL,
-    `reservation_datetime` DATETIME,
-    PRIMARY KEY (`booking_id`) 
+CREATE TABLE IF NOT EXISTS booking (
+    booking_id SERIAL PRIMARY KEY,
+    customer_id VARCHAR(150) NOT NULL,
+    table_id VARCHAR(150) NOT NULL,
+    party_size INT NOT NULL,
+    reservation_datetime TIMESTAMP
 );
