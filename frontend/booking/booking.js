41c5f1fe612c6c3d6b6632f0ef5d74be526f9bc6
Hamra <hamra.khan.b@gmail.com>
2023-01-26 20:45:58 +0000

    - Added function to use preventDefault method on form validation - Added logic to show error if date is before current date on form and if time is not more than 1hr from current time - Added alert to provide number of guests.

--- frontend/booking/booking.js
+++ frontend/booking/booking.js
@@ -1,17 +1,31 @@
+function submitBooking(event) {
+  event.preventDefault();
+  // Perform other actions here, such as validating the form data
+  validateForm();
+}
+
 //Form validation
 function validateForm() {
   const emailRegex =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-  
+
   const phoneRegex = new RegExp(
     /^(?:(?:(?:00\s?|\+)44\s?)|(?:\(?0))(?:\d{2}\)?\s?\d{4}\s?\d{4}|\d{3}\)?\s?\d{3}\s?\d{3,4}|\d{4}\)?\s?(?:\d{5}|\d{3}\s?\d{3})|\d{5}\)?\s?\d{4,5}|8(?:00[\s-]?11[\s-]?11|45[\s-]?46[\s-]?4\d))(?:(?:[\s-]?(?:x|ext\.?\s?|\#)\d+)?)$/
   );
-    
+
   const firstName = document.bookingForm.fname.value;
   const lastName = document.bookingForm.lname.value;
   const email = document.bookingForm.email.value;
   const phone = document.bookingForm.phone.value;
   const guests = document.bookingForm.guests.value;
+
+  const todaysDate = new Date();
+  const selectedDateStr = document.bookingForm.datePicker.value;
+  const selectedTimeStr = document.bookingForm.time.value;
+
+  const selectedDateTime = Date.parse(
+    `${selectedDateStr}T${selectedTimeStr}:00.000Z`
+  );
 
   if (firstName === "") {
     alert("Please provide a first name");
@@ -38,20 +52,12 @@ function validateForm() {
     document.bookingForm.phone.focus();
     return false;
   }
-  if (!phoneRegex.test(selectPhone)) {
+  if (!phoneRegex.test(phone)) {
     alert("Please provide your phone number");
     document.bookingForm.phone.focus();
     return false;
   }
-  if (guests === 0) {
-    alert("Please provide number of guests");
-    document.bookingForm.guests.focus();
-    return false;
-  }
 
-  const todaysDate = new Date();
-  const selectedDateStr = document.bookingForm.datePicker.value;
-  const selectedTimeStr = document.bookingForm.time.value;
   //if date is in date format then show error message
   if (selectedDateStr === "") {
     alert("Please select valid booking date");
@@ -65,23 +71,24 @@ function validateForm() {
     return false;
   }
 
-  const selectedDateTime = Date.parse(
-    `${selectedDateStr}T${selectedTimeStr}:00`
-  );
   //if date is before current date then show error
-  if (selectedDateTime.getTime() < todaysDate.getTime()) {
+  if (selectedDateTime < todaysDate.getTime()) {
     alert("Please select valid booking date");
     document.bookingForm.datePicker.focus();
     return false;
   }
 
   //if time is !> 1hr from current time then show error
-  if (
-    selectedDateTime.getTime() >
-    todaysDate.setHours(todaysDate.getHours() + 1).getTime()
-  ) {
+  const todaysDatePlusOneHour = todaysDate.setHours(todaysDate.getHours() + 1)
+
+  if (!(selectedDateTime > todaysDatePlusOneHour)) {
     alert("Please select valid booking time");
     document.bookingForm.time.focus();
+    return false;
+  }
+  if (!(parseInt(guests) > 0)) {
+    alert("Please provide number of guests");
+    document.bookingForm.guests.focus();
     return false;
   }
 }
